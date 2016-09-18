d0d5c22b3a3e99e3deb8335673816a332a96b8a3
justanothermanoj <justanothermanoj@gmail.com>
2016-09-18 13:41:51 +0530

    Added bundle task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var babelify = require('babelify');
 
 gulp.task('bundle', function(){
 	return browserify({
 		extensions: ['.js', '.jsx'],
 		entries: 'main.js'
 	})
+	.transform(babelify.configure({
+		ignore: /(bower_components)|(node_modules)/
+	}))
+	.bundle()
+	.on("error", function(err){
+		console.log("Error " + err.message);
+	})
+	.pipe(source('bundle.js'))
+	.pipe(gulp.dest('dist'));
 });
