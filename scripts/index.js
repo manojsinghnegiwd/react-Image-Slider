b3d7ba1a43770e9a618ae8f16b9c212151f3c594
justanothermanoj <justanothermanoj@gmail.com>
2016-09-18 17:35:07 +0530

    moved images from state to user provided array

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import {render} from 'react-dom';
 import ReactImageSlider from './react-image-slider';
 
+let images = [
+				{
+					src: './img/bridge-1660417_640.jpg'
+				},
+				{
+					src: './img/milky-way-1655504_640.jpg'
+				},
+				{
+					src: './img/sunset-1670219_640.jpg'
+				},
+				{
+					src: './img/boot-1620452_640.jpg'
+				}
+			];
+
 render(
-  <ReactImageSlider width="640"  height="486" />,
+  <ReactImageSlider width="640"  height="486" images={images} />,
   document.getElementById('root')
 );
--- scripts/react-image-slider.js
+++ scripts/react-image-slider.js
@@ -5,20 +5,6 @@ export default class ReactImageSlider extends Component {
 	constructor(props) {
 		super(props);
 	    this.state = {
-			images: [
-				{
-					src: './img/bridge-1660417_640.jpg'
-				},
-				{
-					src: './img/milky-way-1655504_640.jpg'
-				},
-				{
-					src: './img/sunset-1670219_640.jpg'
-				},
-				{
-					src: './img/boot-1620452_640.jpg'
-				}
-			],
 			currentIndex: 0
 	    };
 	    this.next = this.next.bind(this);
@@ -28,7 +14,7 @@ export default class ReactImageSlider extends Component {
 	next() {
 		let Index = this.state.currentIndex;
 
-		if(Index < this.state.images.length - 1) {
+		if(Index < this.props.images.length - 1) {
 			Index += 1;
 		}
 
@@ -53,12 +39,12 @@ export default class ReactImageSlider extends Component {
 		return (
 		// Add your component markup and other subcomponent references here.
 			<div className="react-image-slider">
-				<img width={this.props.width} height={this.props.height} className="window" onClick={this.next} src={this.state.images[this.state.currentIndex].src}></img>
+				<img width={this.props.width} height={this.props.height} className="window" onClick={this.next} src={this.props.images[this.state.currentIndex].src}></img>
 				<div>
 					<button disabled={this.state.currentIndex == 0} className="buttons" onClick={this.previous}>
 						<i className="fa fa-angle-left"></i>
 					</button>
-					<button disabled={this.state.currentIndex == this.state.images.length -1} className="buttons" onClick={this.next}>
+					<button disabled={this.state.currentIndex == this.props.images.length -1} className="buttons" onClick={this.next}>
 						<i className="fa fa-angle-right"></i>
 					</button>
 				</div>
